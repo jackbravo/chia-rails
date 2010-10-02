bde7c82c0ae23c59882ce70eea46fb33d8f2dc07
Joaquin Bravo Contreras <jackbravo@gmail.com>
2010-10-02 15:04:35 -0500

    Tie tags and entities, also destroy emails when deleting entities

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,5 +1,8 @@
 class Entity < ActiveRecord::Base
   validates :name, :presence => true
 
-  has_many :emails
+  has_many :emails, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
